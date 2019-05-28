d6b824394f4db83ef6ee51bc0d4ad9aafedbf124
franco_vega@msn.com <frankvega2010>
2019-05-28 12:57:36 -0300

    Added sdl test program

---
+++ sdlTest/main.cpp
@@ -0,0 +1,57 @@
+// Example program:
+// Using SDL2 to create an application window
+
+#include "SDL/SDL.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+	SDL_Window *window;                    // Declare a pointer
+
+	SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+
+	// Create an application window with the following settings:
+	window = SDL_CreateWindow(
+		"An SDL2 window",                  // window title
+		SDL_WINDOWPOS_UNDEFINED,           // initial x position
+		SDL_WINDOWPOS_UNDEFINED,           // initial y position
+		640,                               // width, in pixels
+		480,                               // height, in pixels
+		SDL_WINDOW_OPENGL                  // flags - see below
+	);
+
+	// Check that the window was successfully created
+	if (window == NULL) {
+		// In the case that the window could not be made...
+		printf("Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	// The window is open: could enter program loop here (see SDL_PollEvent())
+
+	bool keep_window_open = true;
+	while (keep_window_open)
+	{
+		SDL_Event e;
+		while (SDL_PollEvent(&e) > 0)
+		{
+			switch (e.type)
+			{
+			case SDL_QUIT:
+				keep_window_open = false;
+				break;
+			}
+
+			SDL_UpdateWindowSurface(window);
+		}
+	}
+
+	//SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+	// Close and destroy the window
+	SDL_DestroyWindow(window);
+
+	// Clean up
+	SDL_Quit();
+	return 0;
+}
